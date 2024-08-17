1ce5fdaa8920ea582d320d0f6b7377679d961d52
Filip Markovic <filip98122@gmail.com>
2024-08-17 17:17:24 +0200

    hitpoints ( hp ) next

--- main.py
+++ main.py
@@ -77,9 +77,15 @@ class Knight:
         self.height = self.scaled_img.get_height()
         self.immobilis = 0
         self.last_direction = 1
-        self.state = 0
+        self.state = 3
+        self.jump = 0
+        self.cooldown = 0
+        self.airattack = 0
     def draw(self,window):
-        self.sprite_img = pygame.image.load('idle.png')
+        if self.last_direction == 1:
+            self.sprite_img = pygame.image.load('idle.png')
+        else:
+            self.sprite_img = pygame.image.load('idle_l.png')
         if self.move_l:
             if self.state <= 180 and self.state > 135:
                 self.sprite_img = pygame.image.load('run_1_l.png')
@@ -118,17 +124,30 @@ class Knight:
                 self.sprite_img = pygame.image.load('attack_3_l.png')
             else:
                 self.sprite_img = pygame.image.load('attack_3.png')
+        if self.jump != 0:
+            if self.airattack != 0:
+                if self.last_direction == 1:
+                    self.sprite_img = pygame.image.load('attack_2.png')
+                else:
+                    self.sprite_img = pygame.image.load('attack_2_l.png')
+            else:
+                if self.last_direction == 1:
+                    self.sprite_img = pygame.image.load('run_2.png')
+                else:
+                    self.sprite_img = pygame.image.load('run_2_l.png')
+        
         
         
         if self.sprite_img == pygame.image.load('idle.png'):
-            self.state =0
+            self.state =3
+            
+            
         self.width = self.sprite_img.get_width()*self.scale
         self.height = self.sprite_img.get_height()*self.scale
         self.scaled_img = pygame.transform.scale(self.sprite_img, (self.width, self.height))
         window.blit(self.scaled_img,(self.x,self.y))
         if self.immobilis<=120 and self.immobilis>= 90:
-            self.y += 40
-            
+            self.y += 40        
     def move(self):
         global WIDTH
         global HEIGHT
@@ -138,50 +157,152 @@ class Knight:
         self.dy = 0
         self.move_l = False
         self.move_r = False
-        if self.x >=27.5:
-            if keys[pygame.K_a] or keys[pygame.K_LEFT]:
-                self.dx = -2.5
-                self.move_l =True
-                self.last_direction = -1
-                if self.state == 0:
-                    self.state = 180
-                
-        if self.x <= WIDTH-230-27.5:
-            if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
-                self.dx = 2.5
-                self.move_r = True
-                self.last_direction = 1
-                if self.state == 0:
-                    self.state = 180
-                
+        if self.immobilis==0:
+            if self.x >=27.5:
+                if keys[pygame.K_a] or keys[pygame.K_LEFT]:
+                    self.dx = -2.5
+                    self.move_l =True
+                    self.move_r = False
+                    self.last_direction = -1
+                    if self.state == 3:
+                        self.state = 180
+                    
+            if self.x <= WIDTH-230-27.5:
+                if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
+                    self.dx = 2.5
+                    self.move_l =False
+                    self.move_r = True
+                    self.last_direction = 1
+                    if self.state == 3:
+                        self.state = 180
+        if self.jump == 0:
+            self.y = 550
+            if self.cooldown == 0:
+                if self.immobilis == 0:
+                    if keys[pygame.K_SPACE]:
+                        self.jump = 50
+        
         if self.immobilis == 0:
             if mouseState[0] == True:
-                self.immobilis = 120
+                if self.jump == 0:
+                    self.immobilis = 120
+                else:
+                    self.airattack = 30
+                    
+                    
+        if self.jump>=26:
+            self.jump-=1
+            self.dy=-3
+
+        
+        if self.jump==1:
+            self.cooldown = 30
+        
+        if self.jump>0 and self.jump<=25:
+            self.jump-=1
+            self.dy=3
         
         if self.state != 0:
             if self.immobilis == 0:
                 self.x+=self.dx
                 self.y+=self.dy
+                
         
+        if self.airattack > 0:
+            self.airattack -= 1
         
         if self.immobilis >0:
             self.immobilis -=1
         
+        if self.cooldown >0:
+            self.cooldown-=1
         
-        if self.state > 0:
+        if self.state >= 6:
             self.state-=3
-class Zombie:
-    def __init__(self,x,y,dx,dy):
+class Goblin:
+    def __init__(self,x,y,dx,dy,last_direction):
         self.x = x
         self.y = y
         self.dx = dx
         self.dy = dy
-        self.scale = 1.6
-        self.sprite_img = pygame.image.load('idle.png')
-        self.width = self.sprite_img.get_width()*self.scale
-        self.height = self.sprite_img.get_height()*self.scale
+        self.last_direction = last_direction
+        self.scale = 2
+        self.state = 0
+        self.attack = 0
+        sprite_img = pygame.image.load('run_g1.png')
+        self.width = sprite_img.get_width()*self.scale
+        self.height = sprite_img.get_height()*self.scale
     def draw(self,window):
-        pass
+        sprite_img = 0
+        if self.state <= 180 and self.state > 135:
+            if self.last_direction == 1:
+                sprite_img = pygame.image.load('run_g1.png')
+            else:
+                sprite_img = pygame.image.load('run_g1l.png')
+        if self.state <= 135 and self.state > 90:
+            if self.last_direction == 1:
+                sprite_img = pygame.image.load('run_g2.png')
+            else:
+                sprite_img = pygame.image.load('run_g2l.png')
+        if self.state <= 90 and self.state > 45:
+            if self.last_direction == 1:
+                sprite_img = pygame.image.load('run_g3.png')
+            else:
+                sprite_img = pygame.image.load('run_g3l.png')
+        if self.state <= 45 and self.state > 0:
+            if self.last_direction == 1:
+                sprite_img = pygame.image.load('run_g4.png')
+            else:
+                sprite_img = pygame.image.load('run_g4l.png')
+                
+        if self.attack <= 205 and self.attack > 180:
+            if self.last_direction == 1:
+                sprite_img = pygame.image.load('attack_g1.png')
+            else:
+                sprite_img = pygame.image.load('attack_g1l.png')
+        if self.attack <= 180 and self.attack > 90:
+            if self.last_direction == 1:
+                sprite_img = pygame.image.load('attack_g2.png')
+            else:
+                sprite_img = pygame.image.load('attack_g2l.png')
+        if self.attack <= 90 and self.attack > 45:
+            if self.last_direction == 1:
+                sprite_img = pygame.image.load('attack_g3.png')
+            else:
+                sprite_img = pygame.image.load('attack_g3l.png')
+        if self.attack <= 45 and self.attack > 0:
+            if self.last_direction == 1:
+                sprite_img = pygame.image.load('attack_g4.png')
+            else:
+                sprite_img = pygame.image.load('attack_g4l.png')
+                
+                
+        self.width = sprite_img.get_width()*self.scale
+        self.height = sprite_img.get_height()*self.scale
+        scaled_img = pygame.transform.scale(sprite_img, (self.width, self.height))
+        window.blit(scaled_img,(self.x,self.y))
+    def move(self,kx,kwidth):
+        self.dx = 0
+        if self.attack==0:
+            if kx>self.x:
+                self.last_direction = 1
+                self.dx = 1.5
+            else:
+                self.last_direction = -1
+                self.dx = -1.5
+        if self.state >= 3:
+            self.state -= 3
+        if self.state == 0:
+            self.state = 180
+        if self.attack > 0:
+            self.attack-=1
+        if self.attack == 0:
+            if self.x+self.width>= kx and self.x<kx and self.last_direction == 1 or self.x <= kx+kwidth and self.x>kx and self.last_direction == -1:
+                self.attack = 205
+        self.x += self.dx
+l_g = []
+g1 = Goblin(900,610,0,0,0)
+l_g.append(g1)
 k1 = Knight(100,550,0,0)
 screen = 0
 while True:
@@ -193,12 +314,14 @@ while True:
     mousePos = pygame.mouse.get_pos()
     for event in events:
         if event.type == pygame.QUIT:
-            #ens()
             exit()
     if keys[pygame.K_ESCAPE]:
-        #ens()ddddddddddd
         exit()
+    for i in range(len(l_g)):
+        l_g[i].move(k1.x,k1.width)
     k1.move()
+    for i in range(len(l_g)):
+        l_g[i].draw(window)
     k1.draw(window)
     pygame.display.update()
     clock.tick(60)
